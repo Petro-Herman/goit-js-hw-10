d2dc407fa7887bfa97b0a329da6a1788bd92d4d4
Petro-Herman <germanpetr24@gmail.com>
2024-06-26 00:09:42 +0300

    с1

---
+++ src/js/1-timer.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  flatpickr('#datetime-picker', {});
+});
+
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
