04714ec364605334f60df07c287728c1c48da9bd
Petro-Herman <159651446+Petro-Herman@users.noreply.github.com>
2024-06-25 21:09:57 +0000

    Deploying to gh-pages from @ Petro-Herman/goit-js-hw-10@d2dc407fa7887bfa97b0a329da6a1788bd92d4d4 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-2b44ac2e.js";document.addEventListener("DOMContentLoaded",function(){t("#datetime-picker",{})});
+//# sourceMappingURL=commonHelpers.js.map
