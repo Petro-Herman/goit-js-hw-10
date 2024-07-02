3b689ae5f8679a25c3963f7922169aa49a9479d3
Petro-Herman <germanpetr24@gmail.com>
2024-07-03 00:17:09 +0300

    good

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,32 @@
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = document.querySelector('input[name="delay"]').value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
